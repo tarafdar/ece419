b156e26ba6c6a143f89a0ea9238bbe290d97da3c
Zannier  Jordan Ryan <zannierj@ug175.eecg>
2014-03-28 12:46:45 -0400

    packet

---
+++ lab4/code/src/ClientPacket.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class ClientPacket implements Serializable {
+    
+    //possible request types
+    public static final int JOB_CREATE = 0;
+    public static final int JOB_STATUS = 1;
+    //possible return statuses
+    public static final int JOB_NOT_FOUND = 2;
+    public static final int JOB_IN_PROG = 3;
+    public static final int JOB_FINISHED = 4;
+    //possible pass found values
+    public static final int PASS_FOUND = 5;
+    public static final int PASS_NOT_FOUND = 6;
+
+    public String hash;
+    public int requestType;
+    public int returnStatus;
+    public int passFound;
+    public String password;
+   
+    public ClientPacket(String hash, int req) {
+        this.hash = hash;
+        this.requestType = req;
+    } 
+}
