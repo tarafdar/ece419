8a5674cbccd22889bf8237fb925272c16cf13b58
Zannier  Jordan Ryan <zannierj@ug175.eecg>
2014-03-28 13:26:18 -0400

    should echo

--- lab4/code/src/JobTrackerHandlerThread.java
+++ lab4/code/src/JobTrackerHandlerThread.java
@@ -11,20 +11,25 @@ public class JobTrackerHandlerThread extends Thread {
 
     public void run() {
         ClientPacket packetFromClient;
-        
+        ClientPacket packetToClient; 
         boolean listening = true;
         try {
             ObjectInputStream fromClient = new ObjectInputStream(socket.getInputStream());
 			ObjectOutputStream toClient = new ObjectOutputStream(socket.getOutputStream());
-        
+            
             while(listening) {
                 packetFromClient = (ClientPacket) fromClient.readObject();
+                packetToClient = packetFromClient;
+
                 if(packetFromClient.requestType == ClientPacket.JOB_SUBMIT) {
-                    
+                //create new job
+                    System.out.println("Recieved Job submit with hash " + packetFromClient.hash);
                 }
                 if(packetFromClient.requestType == ClientPacket.JOB_QUERY) {
-                    
+                //check the status of job
+                    System.out.println("Recieved Job query with hash " + packetFromClient.hash);
                 }
+                toClient.writeObject(packetToClient);
             
             }    
 
