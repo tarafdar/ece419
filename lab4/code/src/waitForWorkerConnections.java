93bb2c521d588fbd822355bb3c91850c36011f75
Zannier  Jordan Ryan <zannierj@ug159.eecg>
2014-04-02 16:44:19 -0400

    mystuff

---
+++ lab4/code/src/waitForWorkerConnections.java
@@ -0,0 +1,57 @@
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException.Code;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.*;
+
+import java.net.*;
+
+import java.io.IOException;
+
+public class waitForWorkerConnections extends Thread {
+
+    private ServerSocket workerSocket;
+    private ArrayList<JobPacket> parentJobs;
+    private ArrayList<JobPacket> childJobs;
+    private int numPartitions;
+
+    //public List <String> workers;
+    //private ArrayList <ObjectInputStream> inStreamArray;
+    //private ArrayList <ObjectOutputStream> outStreamArray;
+    
+    public waitForWorkerConnections (ServerSocket workerSocket, ArrayList<JobPacket> parentJobs, ArrayList<JobPacket> childJobs, int numPartitions) {
+        super("waitForWorkerConnections");
+        this.workerSocket = workerSocket;
+        this.parentJobs = parentJobs;
+        this.childJobs = childJobs;
+        this.numPartitions = numPartitions;
+        //this.inStreamArray = inStreamArray;
+        //this.outStreamArray = outStreamArray;
+    }        
+    
+     
+    public void run() {
+       boolean listening = true;
+       while (listening) {
+            try{
+                Socket socket = workerSocket.accept();
+                new workerHandlerThread(socket, parentJobs, childJobs, numPartitions).start();
+            } catch (IOException e) {
+                System.err.println("ERROR: Couldn't get I/O for the connection.");
+		        e.printStackTrace();
+                System.exit(1);
+            }
+       }       
+    }
+}  
---
+++ lab4/code/src/workerHandlerThread.java
@@ -0,0 +1,82 @@
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.KeeperException.Code;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.data.Stat;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import java.net.*;
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.concurrent.CountDownLatch;
+
+public class workerHandlerThread extends Thread {
+    private Socket socket;
+    private ArrayList<JobPacket> parentJobs;
+    private ArrayList<JobPacket> childJobs;
+    private int numPartitions;
+
+    public workerHandlerThread(Socket socket, ArrayList<JobPacket> parentJobs, ArrayList<JobPacket> childJobs, int numPartitions) {
+        super("workerHandlerThread");
+        this.socket = socket;
+        this.parentJobs = parentJobs;
+        this.childJobs = childJobs;
+        this.numPartitions = numPartitions;
+    }
+
+    public void run() {
+        JobPacket packetFromWorker = null;
+        JobPacket packetToWorker = null; 
+        boolean listening = true;
+        try {
+            ObjectInputStream fromWorker = new ObjectInputStream(socket.getInputStream());
+			ObjectOutputStream toWorker = new ObjectOutputStream(socket.getOutputStream());
+            
+            while(listening) {
+                packetToWorker = null;
+                synchronized(childJobs) {
+                    if(childJobs.size() > 0) {
+                        packetToWorker = childJobs.remove(0);
+                    }
+                }
+                if (packetToWorker != null) {
+                    toWorker.writeObject(packetToWorker);
+                    packetFromWorker = (JobPacket) fromWorker.readObject();
+                    int i;
+                    if(packetFromWorker.found) {
+                        synchronized(parentJobs) {
+                            for(i=0; i<parentJobs.size(); i++) {
+                                if(packetFromWorker.hash.equals(parentJobs.get(i).hash)) {
+                                    parentJobs.get(i).done = true;
+                                    parentJobs.get(i).found = true;
+                                }    
+                            }    
+                        }        
+                    }
+                    else {
+                        synchronized(parentJobs) {
+                            for(i=0; i<parentJobs.size(); i++) {
+                                if(packetFromWorker.hash.equals(parentJobs.get(i).hash)) {
+                                    parentJobs.get(i).partitionsCompleted++;
+                                    if(parentJobs.get(i).partitionsCompleted == numPartitions)
+                                        parentJobs.get(i).done = true;
+                                }        
+                            }    
+                        }        
+                    }         
+                }    
+            }    
+
+		} catch (IOException e) {
+		    //e.printStackTrace();
+            listening = false;
+            childJobs.add(packetToWorker);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+            listening = false;
+		}
+    }
+}
